1245670d45a84c57f9e583220715bf789fe3c8b1
Elliot <elliot@oxygenproductions.com>
2012-08-29 19:39:05 -0400

    fix for ie placeholders & a img borders

--- src/main/webapp/scripts/common.js
+++ src/main/webapp/scripts/common.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
   
+  $("'[placeholder]'").focus(function() {
+    var input = $(this);
+    if (input.val() == input.attr("'placeholder'")) {
+      input.val("''");
+      input.removeClass("'placeholder'");
+    }
+  }).blur(function() {
+    var input = $(this);
+    if (input.val() == "''" || input.val() == input.attr("'placeholder'")) {
+      input.addClass("'placeholder'");
+      input.val(input.attr("'placeholder'"));
+    }
+  }).blur();
+  
+  
   var controller = $.scrollorama({
     blocks: '.hero',
     enablePin: false
